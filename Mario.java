5f60a853ea7fc13d3cb306de7fee29e3d718d4b0
Jacob Zimmerman <ijacobzim@gmail.com>
2017-02-22 14:47:58 -0800

    Cleaned up some code

--- Mario.java
+++ Mario.java
@@ -4,13 +4,11 @@ public class Mario {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int height;
-		do{
-		System.out.println("Enter a height within 0 and 23");
-		height = s.nextInt();
-		} while (height < 0 || height > 23);{
-			System.out.println("Please enter an integer within 0 and 23");
-		}
+		int height = 0;
+		do {
+			  System.out.println("Enter a height within 0 and 23");
+			  height = s.nextInt();
+			} while (height < 0 || height > 23);
 		char[] line = new char[height+1];
 		for(int i = 0; i < height; i++){
 			for(int j = height; j >= 0; j--){
