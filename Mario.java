7dec3a0eb3b955c80b72bbe163f313315d42ea84
Jacob Zimmerman <ijacobzim@gmail.com>
2017-02-22 15:38:56 -0800

    Added user option to save as txt file

--- Mario.java
+++ Mario.java
@@ -1,14 +1,25 @@
 import java.util.Scanner;
-
+import java.io.*;
 public class Mario {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int height = 0;
+		String cslOrTxt;
+		while (true) {
+			System.out.println("Would you like to save a txt file or print to console? \n"
+					+ "(csl) (txt)");
+			cslOrTxt = s.nextLine();
+			if (!((cslOrTxt.equals("csl")) || (cslOrTxt.equals("txt")))){
+				System.out.println("Please enter either txt (txt file) or csl (print to console)");
+				continue;
+			} else break;
+		}
 		do {
 			  System.out.println("Enter a height within 0 and 23");
 			  height = s.nextInt();
 			} while (height < 0 || height > 23);
+		String[] lines = new String[height];
 		char[] line = new char[height+1];
 		for(int i = 0; i < height; i++){
 			for(int j = height; j >= 0; j--){
@@ -17,7 +28,28 @@ public class Mario {
 				} else {
 					line[j] = '#';
 				}
-			} System.out.println(line);
+			}
+			if (cslOrTxt.equals("csl")){ 
+				System.out.println(line);
+			} else {
+				lines[i] = new String(line);
+			}
+		}
+		if (cslOrTxt.equals("txt")) {
+			try {
+			Scanner m = new Scanner(System.in);
+			String path;
+			System.out.println("Please enter the directory for this file (will save as Mario.txt)");
+			path = m.nextLine();
+			PrintWriter writer = new PrintWriter(path + "\\"+"Mario.txt", "UTF-8");
+			for (int l = 0; l < height; l++){
+				writer.println(lines[l]);
+			}
+			writer.close();
+			System.out.println("Txt File Saved!");
+			} catch (Exception e){
+				System.out.println("looks like the path you entered doesn't exist!");
+			}
 		}
 	}
 }
