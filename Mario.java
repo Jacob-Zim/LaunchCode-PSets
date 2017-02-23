9093ed47278e7a26ba82603c20f5d322917b33f9
Jacob Zimmerman <ijacobzim@gmail.com>
2017-02-22 20:44:00 -0800

    configured with dependency injection

--- Mario.java
+++ Mario.java
@@ -2,11 +2,21 @@ import java.util.Scanner;
 import java.io.*;
 public class Mario {
 
-	public static void main(String[] args) {
-		Pyramid.getInstance().cslOrTxt();
-		Pyramid.getInstance().height();
-		String cslOrTxt = Pyramid.getInstance().cslOrTxt;
-		int h = Pyramid.getInstance().h;
-		Pyramid.createPyramid(h, cslOrTxt);
-		}
+	private Pyramid pyramid;
+
+	public Mario(Pyramid pyramid) {
+		// TODO Auto-generated constructor stub
+		this.pyramid = pyramid;
 	}
+
+	public static void main(String[] args) {
+		Mario mario = new Mario(Pyramid.getInstance());
+		mario.run();
+	}
+	
+	public void run(){
+		pyramid.cslOrTxt();
+		pyramid.height();
+		Pyramid.createPyramid(pyramid.h, pyramid.cslOrTxt);
+	}
+}
